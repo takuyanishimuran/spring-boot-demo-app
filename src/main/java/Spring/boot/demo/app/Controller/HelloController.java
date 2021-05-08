677564abbc6d4baef972e979f9621df3cbdca76f
takuya-nishimura-01 <takuya.nishimura.chel@gmail.com>
2021-05-08 16:12:32 +0900

    change RestController -> Controller

--- src/main/java/Spring/boot/demo/app/Controller/HelloController.java
+++ src/main/java/Spring/boot/demo/app/Controller/HelloController.java
@@ -1,13 +1,15 @@
 package Spring.boot.demo.app.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class HelloController {
 
-  @RequestMapping("/hello")
-  String hello() {
+  @GetMapping("/hello")
+  @ResponseBody
+  public String hello() {
     return "Hello World!!!";
   }
 
