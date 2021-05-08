fc27599e1a44b99a3aa5e62517218788a315ce42
takuya-nishimura-01 <takuya.nishimura.chel@gmail.com>
2021-05-08 16:54:55 +0900

    add lombok

--- src/main/java/Spring/boot/demo/app/Controller/HelloController.java
+++ src/main/java/Spring/boot/demo/app/Controller/HelloController.java
@@ -1,16 +1,29 @@
 package Spring.boot.demo.app.Controller;
 
+import Spring.boot.demo.app.Logic.SetLogic;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class HelloController {
+
+  private final SetLogic setLogic;
 
   @GetMapping("/hello")
   @ResponseBody
   public String hello() {
     return "Hello World!!!";
+  }
+
+  @GetMapping(value = "/set")
+  @ResponseBody
+  public String set(@RequestParam(name = "name", defaultValue = "デフォルト値") String name) {
+
+    return setLogic.addPrefix(name);
   }
 
 }
