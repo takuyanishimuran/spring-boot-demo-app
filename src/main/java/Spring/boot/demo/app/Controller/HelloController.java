23bc59ae42aa731ecf9872ddc8b0fdf1a483127c
takuya-nishimura-01 <takuya.nishimura.chel@gmail.com>
2021-05-09 12:51:35 +0900

    add logback.xml

--- src/main/java/Spring/boot/demo/app/Controller/HelloController.java
+++ src/main/java/Spring/boot/demo/app/Controller/HelloController.java
@@ -2,11 +2,13 @@ package Spring.boot.demo.app.Controller;
 
 import Spring.boot.demo.app.Logic.SetLogic;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HelloController {
@@ -22,6 +24,8 @@ public class HelloController {
   @GetMapping(value = "/set")
   @ResponseBody
   public String set(@RequestParam(name = "name", defaultValue = "デフォルト値") String name) {
+
+    log.info("test log-Slf4j");
 
     return setLogic.addPrefix(name);
   }
